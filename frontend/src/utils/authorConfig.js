e04df5ef3f0b0cecfb294609756028b7129966d2
Prajwal0422 <prajwal0422@users.noreply.github.com>
2026-05-20 17:54:19 +0530

    feat: Add author configuration - Prajwal0422

---
+++ frontend/src/utils/authorConfig.js
@@ -0,0 +1,11 @@
+/**
+ * Author Configuration
+ * Portfolio by Prajwal0422
+ */
+
+export const AUTHOR = {
+  name: 'Prajwal Jain',
+  username: 'Prajwal0422',
+  email: 'prajwal0422@users.noreply.github.com',
+  github: 'https://github.com/Prajwal0422',
+};
